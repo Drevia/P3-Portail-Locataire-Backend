f0c19d324532777cdd666e63c9e5897d4000ed46
Cyriac <drevia59@gmail.com>
2025-11-17 13:51:32 +0100

    Initial commit

---
+++ src/main/java/com/openclassrooms/rental/PortailLocataireBackApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortailLocataireBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortailLocataireBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/rental/PortailLocataireBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortailLocataireBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
